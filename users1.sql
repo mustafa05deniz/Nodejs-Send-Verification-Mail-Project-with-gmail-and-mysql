8102d20bef2786f36e5d35ff61d6ac6aedb0ded3
Mustafa Deniz <mustafa05deniz@gmail.com>
2017-08-08 19:25:55 +0300

    sql
    sql

---
+++ users1.sql
@@ -0,0 +1,10 @@
+﻿
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `verification` tinyint(1) DEFAULT '0',
+  `code` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
